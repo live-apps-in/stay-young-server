217bbc2ac75a399a72448a9cbbdb2a3aa2333325
iam-deepakvel <deepakvel82@gmail.com>
2023-04-20 19:18:02 +0530

    Added appconfig

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+import helmet from 'helmet';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  app.use(express.json());
+  app.use(helmet());
   await app.listen(3000);
 }
+
 bootstrap();
