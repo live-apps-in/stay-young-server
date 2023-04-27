fe36de18e25e4e0b4ce9c7176c40d1554d3795b0
iam-deepakvel <deepakvel82@gmail.com>
2023-04-27 13:23:25 +0530

    fix - swagger url

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
   app.enableCors();
   app.use(helmet());
   await app.listen(process.env.PORT || 3000);
